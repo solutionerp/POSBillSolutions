78010d457b04b5d99af4f267d27ea27ef52c4d67
solutionerp <solutionerpminttechnologyllp@gmail.com>
2023-03-21 10:44:24 +0530

    Framework
    POSBill EntityFramework Project

---
+++ POSBill.EntityFramework/RestaurantRetailPOSBillDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POSBill.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSBill.EntityFramework
+{
+    public class RestaurantRetailPOSBillDBContext :DbContext
+    {
+       
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server.\\DESKTOP-LETTAVT;Database=RRDB;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    } 
+}
