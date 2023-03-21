ceb072587810897fa5fd11aff692c98e47972bb7
solutionerp <solutionerpminttechnologyllp@gmail.com>
2023-03-21 10:43:24 +0530

    Domain Project
    POSBill Domain Project

---
+++ POSBill.Domain/Services/IDataServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSBill.Domain.Services
+{
+    public interface IDataServices<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T entity);
+        Task<T> Update(int id, T Enitity);
+        Task<bool>Delete(int id);
+    }
+}
