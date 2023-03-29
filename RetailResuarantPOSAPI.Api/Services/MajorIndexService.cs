0a386b2f9bb10d6805ad7cd0a5c33225e26e486f
solutionerp <solutionerpminttechnologyllp@gmail.com>
2023-03-29 14:56:56 +0530

    API Project
    Dll for API calling

---
+++ RetailResuarantPOSAPI.Api/Services/MajorIndexService.cs
@@ -0,0 +1,36 @@
+﻿using POSBill.Domain.Models;
+using POSBill.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailResuarantPOSAPI.Api.Services
+{
+    public class MajorIndexService : IMajorIndexService
+    {
+        public async Task<String> Authenticate(string username, string password)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var requestContent = new FormUrlEncodedContent(new[] {
+            new KeyValuePair<string, string>("username", username),
+            new KeyValuePair<string, string>("password", password)
+        });
+
+                var response = await httpClient.PostAsync("http://192.168.0.223/lab/qcc2/modules/mpos_r/api/v3/auth/login", requestContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    throw new Exception($"Authentication failed: {response.StatusCode}");
+                }
+            }
+        }
+    }
+}
+
