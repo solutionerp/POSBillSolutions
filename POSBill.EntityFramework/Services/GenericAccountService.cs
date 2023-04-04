5963fedabbcce3ee95a4cf5905dcdc36dbad0a87
solutionerp <solutionerpminttechnologyllp@gmail.com>
2023-04-04 09:52:40 +0530

    EntityFramework
    EntityFramework

---
+++ POSBill.EntityFramework/Services/GenericAccountService.cs
@@ -0,0 +1,23 @@
+﻿using POSBill.Domain.Models;
+using POSBill.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSBill.EntityFramework.Services
+{
+    public class GenericAccountService<T> : IAccountServices<T> where T : DomainObject
+    {
+        public Task<String> Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
